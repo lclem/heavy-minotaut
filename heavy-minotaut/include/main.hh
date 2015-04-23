b73c7da5096fbccc06708215538d8f186fd208f4
ric-almeida <ricardoalmeida1985@gmail.com>
2015-04-23 16:21:52 +0100

    Update

---
+++ heavy-minotaut/include/main.hh
@@ -0,0 +1,23 @@
+
+#ifndef _MAIN_HH_
+#define _MAIN_HH_
+
+#include <boost/lexical_cast.hpp>
+
+#include "../src/Common/common.hh"
+#include "../src/step.hh"
+//#include "../src/Common/util.hh"
+#include "../src/Common/debugging.hh"
+//#include "../src/Common/combinationsGenerator.hh"
+#include "../src/Common/automatonHelper.hh"
+#include "../src/dw_simulation.hh"
+#include "../src/up_simulation.hh"
+#include "../src/quotienting.hh"
+#include "../src/pruning.hh"
+
+#include "../testers/dw_simulation_tester.hh"
+#include "../testers/minimizationTechniques_tester.hh"
+
+
+
+#endif
